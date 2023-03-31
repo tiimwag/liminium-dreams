e0f3351c2e0d2ec18c6c344b2bb34aecce30c0ce
tiimwag <tsartemenko@edu.hse.ru>
2023-03-31 03:50:38 +0300

    omg

--- javascript/content.js
+++ javascript/content.js
@@ -154,3 +154,103 @@ $(function(){
     $(".anger_two").toggleClass("angerCh2");
   });
 });
+
+// СТРАХ
+$(document).ready(function(){
+  $(".hex").click(function(){
+    $(".fear").css("display", "block");
+    $(".fear").css("opacity", "100%");
+  });
+});
+$(document).ready(function(){
+  $(".fear_return").click(function(){
+    $(".fear").css("opacity", "0");
+    setTimeout(function() {
+      $(".fear").css("display", "none");
+    }, 500);
+    $(".mainscreen").css("display", "block");
+    setTimeout(function() {
+      $(".mainscreen").css("opacity","100%");
+      $(".cursorchik").css("display","block");
+    }, 200);
+  });
+});
+$(document).ready(function(){
+  let f = 0;
+  $(".needle").click(function(){
+    $(this).css("opacity", "0");
+    f += 1;
+    if (f == 9) {
+      $(".fear").css("background-color", "#FFFFFF");
+      $(".pers_circle").css("background-color", "#FF4000");
+      $(".pers_circle").css("animation-play-state", "paused");
+      $(".fear_return").css("color", "#FF4000");
+      $(".fear_return").css("animation-play-state", "paused");
+    }
+  });
+});
+
+// РАДОСТЬ
+$(document).ready(function(){
+  $(".row").click(function(){
+    $(".joy").css("display", "block");
+    $(".joy").css("opacity", "100%");
+  });
+});
+$(document).ready(function(){
+  $(".joy_return").click(function(){
+    $(".joy").css("opacity", "0");
+    setTimeout(function() {
+      $(".joy").css("display", "none");
+    }, 500);
+    $(".mainscreen").css("display", "block");
+    setTimeout(function() {
+      $(".mainscreen").css("opacity","100%");
+      $(".cursorchik").css("display","block");
+    }, 200);
+  });
+});
+$(document).ready(function(){
+  $(".jb1").click(function(){
+    $(".joy").toggleClass("joyRed");
+    $(".joybox").toggleClass("joyboxWhite");
+    $(".joy_return").toggleClass("joy_returnWhite");
+  });
+  j = 0
+  $(".jb2").click(function(){
+    j += 1;
+    if (j == 1) {
+      $(".jb2").addClass("jb2Ch1");
+    }
+    else if (j == 2) {
+      $(".jb2").addClass("jb2Ch2");
+    }
+    else if (j == 3) {
+      $(".jb2").addClass("jb2Ch3");
+    }
+    else {
+      j = 0;
+      $(".jb2").removeClass("jb2Ch1");
+      $(".jb2").removeClass("jb2Ch2");
+      $(".jb2").removeClass("jb2Ch3");
+    }
+  });
+});
+$(function(){
+  $(".jb3").click(function(){
+    $(".canvass").toggleClass("disp");
+    $(".drawing").toggleClass("drawingD");
+  });
+});
+
+function setup() {
+  createCanvas(1920, 1080);
+  background(255);
+}
+
+function draw() {
+  stroke(255, 64, 0);
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
